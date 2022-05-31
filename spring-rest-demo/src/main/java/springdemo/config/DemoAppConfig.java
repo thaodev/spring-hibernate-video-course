70016afbad27b82831e3f941deeaf931665c4f06
Thao Nguyen <thaontt1408@gmail.com>
2022-05-31 16:34:07 -0600

    This classh for configuration by Java annotation

---
+++ spring-rest-demo/src/main/java/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("springdemo")
+public class DemoAppConfig {
+
+}
