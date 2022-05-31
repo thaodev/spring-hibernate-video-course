d2f7e2fe22676762b3862dd284748fa2b7de0564
Thao Nguyen <thaontt1408@gmail.com>
2022-05-31 16:35:02 -0600

    Create MVC Dispatcher Servlet Initializer

---
+++ spring-rest-demo/src/main/java/springdemo/config/MySringMvcDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package springdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
