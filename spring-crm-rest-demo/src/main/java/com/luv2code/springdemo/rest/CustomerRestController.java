c529022e39572d25aa62f5a40d4d3f1a362b97dc
Thao Nguyen <thaontt1408@gmail.com>
2022-06-03 07:11:25 -0600

    Create add a new customer or update an existing one under JSON data

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,61 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+		
+	//autowire the CustomerService
+	@Autowired
+	private CustomerService customerService;
+	
+	
+	//add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomerList(){
+		
+		return customerService.getCustomers();
+	}
+	
+	//add mapping for GET/customers/{customerId}
+	@GetMapping("customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
+	}
+	
+	//add mapping for POST/customers to add a new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	//add mapping for PUT/customers to update an existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+}
