1692be452ab86cd12aa5d4ea45d01b0c782f2802
Thao Nguyen <thaontt1408@gmail.com>
2022-06-02 22:17:38 -0600

    add ALTER USER 'springstudent'@'localhost' IDENTIFIED WITH mysql_native_password BY 'springstudent'

--- spring-crm-rest-demo/sql-scripts/01-create-user.sql
+++ spring-crm-rest-demo/sql-scripts/01-create-user.sql
@@ -1,3 +1,7 @@
 CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
 
 GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
+
+#DROP USER 'springstudent'@'localhost';
+
+ALTER USER 'springstudent'@'localhost' IDENTIFIED WITH mysql_native_password BY 'springstudent'
